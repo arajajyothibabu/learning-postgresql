c4f646ef7dd2e2693c760b7450700e0dcb84636b
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-11-18 00:53:48 +0530

    advanced queries

--- queries.sql
+++ queries.sql
@@ -41,4 +41,89 @@ WHERE sqltest.buyer.country = 'US'
   AND sqltest.defect.defect_type_description = 'Delivery Miss'
   AND transaction_date >= to_date('2012-09','YYYY-MM')
   AND transaction_date <= to_date('2015-12','YYYY-MM')
-GROUP BY sqltest.service.name, sqltest.carrier.carrier_name;
+GROUP BY sqltest.service.name, sqltest.carrier.carrier_name
+ORDER BY count(*) DESC;
+
+/**
+-- question 23
+-- In June 2015 considering only those buyers having more than 2 transactions in that month, identify the top leaf category names based on total number of items purchased.
+ */
+SELECT
+  sqltest.transaction.buyer_id, sqltest.category.leaf_category_name, count(*)
+FROM sqltest.transaction
+  INNER JOIN sqltest.category
+    ON sqltest.transaction.leaf_category_id = sqltest.category.leaf_category_id
+WHERE transaction_date = to_date('2015-06','YYYY-MM')
+GROUP BY sqltest.transaction.buyer_id, sqltest.category.leaf_category_name
+HAVING count(*) > 2;
+
+
+/**
+Testing purpose
+
+SELECT
+  sqltest.transaction.seller_id, sqltest.defect.defect_type, sqltest.transaction.transaction_date, count(*)
+FROM sqltest.transaction
+  INNER JOIN sqltest.defect
+    ON sqltest.transaction.defect_id = sqltest.defect.defect_id
+WHERE sqltest.defect.defect_type_description != 'No Defect'
+GROUP BY sqltest.transaction.seller_id, sqltest.defect.defect_type, sqltest.transaction.transaction_date
+ORDER BY sqltest.transaction.transaction_date;
+
+SELECT
+  sqltest.transaction.seller_id as sellerId1, MIN(sqltest.transaction.transaction_date) as firstDay
+FROM sqltest.transaction
+GROUP BY sqltest.transaction.seller_id;
+
+SELECT
+  sqltest.transaction.seller_id as sellerId2, MIN(sqltest.transaction.transaction_date) as secondDay
+FROM sqltest.transaction
+  INNER JOIN sqltest.defect
+    ON sqltest.transaction.defect_id = sqltest.defect.defect_id
+WHERE sqltest.defect.defect_type_description != 'No Defect'
+GROUP BY sqltest.transaction.seller_id;
+*/
+
+/**
+-- question 24
+-- what is the average number of days for a seller to make a defective transaction.
+-- Hint: Average Number of days between the first transaction date and the transaction date with a defect
+ */
+
+SELECT
+  AVG(secondDay - firstDay)
+FROM
+  (
+    SELECT
+     sqltest.transaction.seller_id as sellerId1, MIN(sqltest.transaction.transaction_date) as firstDay
+    FROM sqltest.transaction
+    GROUP BY sqltest.transaction.seller_id
+  )
+  INNER JOIN
+  (
+    SELECT
+     sqltest.transaction.seller_id as sellerId2, MIN(sqltest.transaction.transaction_date) as secondDay
+    FROM sqltest.transaction
+    INNER JOIN sqltest.defect
+      ON sqltest.transaction.defect_id = sqltest.defect.defect_id
+    WHERE sqltest.defect.defect_type_description != 'No Defect'
+    GROUP BY sqltest.transaction.seller_id
+  )
+  ON sellerId1 = sellerId2;
+
+
+/**
+-- question 50
+-- Find the top 3 sellers who had shipped the most number of items within 10 days of transaction in 2015
+ */
+
+SELECT TOP 3
+sqltest.seller.name, count(*)
+FROM sqltest.transaction
+  INNER JOIN sqltest.seller
+    ON sqltest.transaction.seller_id = sqltest.seller.seller_id
+  INNER JOIN sqltest.shipping
+    ON sqltest.transaction.shipping_id = sqltest.shipping.shipping_id
+WHERE sqltest.shipping.seller_ship_date - transaction_date <= 10
+GROUP BY sqltest.seller.name
+ORDER BY count(*) DESC;
